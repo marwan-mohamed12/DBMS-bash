4010bd0660dfe0a5e4e8b1854e5af79554e2d059
Marwan Mohamed <marawanmohamed9876@gmail.com>
2023-12-21 22:50:44 +0200

    feat: added the create, list, insert, delete table functionality

--- script.sh
+++ script.sh
@@ -18,21 +18,107 @@ function DropDb {
     rm -r databases/$1
 }
 
+function createTable {
+    read -p "Enter Table Name: " tableName
+    mkdir $tableName
+    cd $tableName
+
+    touch "${tableName}.txt"
+    touch "${tableName}-meta.txt"
+
+    read -p "Enter Number Of Columns: " cols
+
+    num=0
+    nameRecord=""
+    dataTypeRecord=""
+    while [ $num -lt $cols ]
+    do 
+        read -p "Col Name: " colName
+        read -p "Col Datatype (string or num): " colType
+
+        if [ $num -eq $((cols-1)) ]
+        then
+            nameRecord="${nameRecord}${colName}"
+            dataTypeRecord="${dataTypeRecord}${colType}"
+        else
+            nameRecord="${nameRecord}${colName}:"
+            dataTypeRecord="${dataTypeRecord}${colType}:"
+        fi
+
+        let num=$num+1
+    done
+
+    echo $dataTypeRecord >> "${tableName}-meta.txt"
+    echo $nameRecord >> "${tableName}-meta.txt"
+
+    cd ../
+
+}
+
+function listTables {
+    ls 
+}
+
+function dropTable {
+    read -p "Enter Table name: " rTable
+    rm -r $rTable
+}
+
+function insertTable {
+    read -p "Enter Table Name: " tableName
+    typeset colNum
+    colNum=$( head -1 ${tableName}/${tableName}-meta.txt | awk -F':' '{print NF}')
+    
+    typeset num=0
+    typeset insertVal=""
+    while [ $num -lt $colNum ]
+    do
+        typeset colName=$(tail -1 ${tableName}/${tableName}-meta.txt | cut -d ':' -f $((num+1)))
+        typeset colDatatype=$(head -1 ${tableName}/${tableName}-meta.txt | cut -d ':' -f $((num+1)))
+        read -p "Enter value of ${colName} in ${colDatatype}: " colValue
+        
+        if [ $num -eq $((colNum-1)) ]
+        then
+            insertVal="${insertVal}${colValue}"
+        else
+            insertVal="${insertVal}${colValue}:"
+        fi
+
+        let num=$num+1
+    done
+
+    echo ${insertVal} >> "${tableName}/${tableName}.txt"
+
+}
+
+function deleteRecord {
+    typeset id 
+    typeset tableName
+    read -p "Enter Table Name: " tableName
+    read -p "Enter Id to delete: " id
+    sed -i "/^${id}/d" "${tableName}/${tableName}.txt"
+}
+
 function showTablesMenu {
     select choice2 in "Create Table" "List Tables" "Drop Tables" "Insert" "Select" "Delete" "Update" Quit
         do  
             case $choice2 in
             "Create Table") echo "Create Table"
+            createTable
             ;;
             "List Tables") echo "List Tables"
+            listTables
             ;;
             "Drop Tables") echo "Drop Tables"
+            dropTable
             ;;
             "Insert") echo "Insert"
+            insertTable
             ;;
             "Select") echo "Select"
             ;;
             "Delete") echo "Delete"
+            deleteRecord
             ;;
             "Update") echo "Update"
             ;;
